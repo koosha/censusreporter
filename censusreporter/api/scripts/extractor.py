07d0dc1b7397a1242b261faa401ca401dc1c8186
Koosha <koosha.g@gmail.com>
2015-11-01 16:06:47 -0700

    INSERT census data to DB with disaggregated ages

--- censusreporter/api/scripts/extractor.py
+++ censusreporter/api/scripts/extractor.py
@@ -2,16 +2,15 @@ __author__ = 'kooshag'
 
 
 import pandas as pd
-df = pd.read_csv('tbl_ward.csv')
+df = pd.read_csv('yeg_census_disag.csv')
 df.columns = [c.lower() for c in df.columns]
 
 from sqlalchemy import create_engine
 engine = create_engine('postgresql://census:census@localhost:5432/census_2011')
 
-df.to_sql("tbl_ward", engine)
+df.to_sql("yeg_census_age", engine)
 
 
-print('test')
 
 
 
